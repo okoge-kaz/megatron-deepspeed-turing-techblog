e5fd093fb085d96e66307a242dcee8b32565580b
kazuki <kaz.tokyo.tech20@gmail.com>
2023-07-06 23:31:51 +0900

    feat: mpirun dp4 tp2 pp2

--- scripts/mpirun/345m_dp4_tp2_pp2.sh
+++ scripts/mpirun/345m_dp4_tp2_pp2.sh
@@ -7,14 +7,23 @@ GPUS_PER_NODE=8
 NNODES=2
 WORLD_SIZE=$(($GPUS_PER_NODE * $NNODES))
 
+TP_SIZE=2
+PP_SIZE=2
+DP_SIZE=$(($WORLD_SIZE / ($TP_SIZE * $PP_SIZE)))
+
+echo -e "\nTP_SIZE: $TP_SIZE, PP_SIZE: $PP_SIZE, DP_SIZE: $DP_SIZE\n"
+
 # load virtualenv
 source /model/hpc-team/Megatron-DeepSpeed/.env/bin/activate
 
 # dataset, checkpoint path
 DATA_PATH=dataset/BookCorpusDataset_text_document
-CHECKPOINT_PATH=checkpoints/gpt2_345m/2node-16gpu-mpirun
+CHECKPOINT_PATH=checkpoints/gpt2_345m/${NNODES}node-${WORLD_SIZE}gpu-dp${DP_SIZE}-tp${TP_SIZE}-pp${PP_SIZE}-mpirun
 
 mkdir -p $CHECKPOINT_PATH
+
+MICRO_BATCHSIZE=8
+GLOBAL_BATCHSIZE=$(($MICRO_BATCHSIZE * $DP_SIZE))
 
 
 # Open MPI training
@@ -27,11 +36,13 @@ mpirun -np $WORLD_SIZE --npernode $GPUS_PER_NODE \
   -x NCCL_DEBUG=INFO  -x PATH \
   -mca pml ob1 -mca btl ^openib \
   python pretrain_gpt.py \
+  --tensor-model-parallel-size $TP_SIZE \
+  --pipeline-model-parallel-size $PP_SIZE \
   --num-layers 24 \
   --hidden-size 1024 \
   --num-attention-heads 16 \
-  --micro-batch-size 8 \
-  --global-batch-size 128 \
+  --micro-batch-size $MICRO_BATCHSIZE \
+  --global-batch-size $GLOBAL_BATCHSIZE \
   --seq-length 1024 \
   --max-position-embeddings 1024 \
   --train-iters 500000 \
@@ -59,4 +70,4 @@ mpirun -np $WORLD_SIZE --npernode $GPUS_PER_NODE \
   --use-mpi \
   --log-batch-size-to-tensorboard \
   --log-validation-ppl-to-tensorboard \
-  --wandb-name "gpt2_345m_2node_dp16-mpirun"
+  --wandb-name "gpt2_345m_${NNODES}node_dp${DP_SIZE}-tp${TP_SIZE}-pp${PP_SIZE}-mpirun"
