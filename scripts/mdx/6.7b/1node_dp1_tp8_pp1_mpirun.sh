25e4020deab466fa52d0b5c39c803b5ce873c3f2
kazuki <kaz.tokyo.tech20@gmail.com>
2023-07-13 15:09:01 +0900

    feat: 6.7b job script

---
+++ scripts/mdx/6.7b/1node_dp1_tp8_pp1_mpirun.sh
@@ -0,0 +1,78 @@
+
+# distributed settings
+GPUS_PER_NODE=8
+NNODES=1
+WORLD_SIZE=$(($GPUS_PER_NODE * $NNODES))
+
+TP_SIZE=8
+PP_SIZE=1
+DP_SIZE=$(($WORLD_SIZE / ($TP_SIZE * $PP_SIZE)))
+
+echo -e "\nTP_SIZE: $TP_SIZE, PP_SIZE: $PP_SIZE, DP_SIZE: $DP_SIZE\n"
+
+nvidia-smi nvlink --status
+
+# load virtualenv
+source .env/bin/activate
+
+# dataset, checkpoint path
+DATA_PATH=dataset/BookCorpusDataset_text_document
+CHECKPOINT_PATH=checkpoints/gpt_6.7b/${NNODES}node-${WORLD_SIZE}gpu-dp${DP_SIZE}-tp${TP_SIZE}-pp${PP_SIZE}-mpirun
+
+mkdir -p $CHECKPOINT_PATH
+
+MICRO_BATCHSIZE=1
+GLOBAL_BATCHSIZE=$(($MICRO_BATCHSIZE * $DP_SIZE))
+
+# model parameter (6.7B)
+NUM_LAYERS=32
+HIDDEN_SIZE=4096
+NUM_ATTENTION_HEADS=32
+SEQ_LENGTH=2048
+MAX_POSITION_EMBEDDINGS=2048
+
+
+MASTER_ADDR="10.2.72.141"
+
+mpirun -np $WORLD_SIZE --npernode $GPUS_PER_NODE \
+  -x MASTER_ADDR=$MASTER_ADDR \
+  -x MASTER_PORT=16500 \
+  -bind-to none -map-by slot \
+  -x NCCL_DEBUG=INFO  -x PATH \
+  -mca pml ob1 -mca btl ^openib \
+  python pretrain_gpt.py \
+  --tensor-model-parallel-size $TP_SIZE \
+  --pipeline-model-parallel-size $PP_SIZE \
+  --num-layers $NUM_LAYERS \
+  --hidden-size $HIDDEN_SIZE \
+  --num-attention-heads $NUM_ATTENTION_HEADS \
+  --micro-batch-size $MICRO_BATCHSIZE \
+  --global-batch-size $GLOBAL_BATCHSIZE \
+  --seq-length $SEQ_LENGTH \
+  --max-position-embeddings $MAX_POSITION_EMBEDDINGS \
+  --train-iters 100 \
+  --lr-decay-iters 80 \
+  --save $CHECKPOINT_PATH \
+  --load $CHECKPOINT_PATH \
+  --data-path $DATA_PATH \
+  --vocab-file dataset/gpt2-vocab.json \
+  --merge-file dataset/gpt2-merges.txt \
+  --data-impl mmap \
+  --split 900,50,50 \
+  --distributed-backend nccl \
+  --lr 0.00015 \
+  --lr-decay-style cosine \
+  --min-lr 1.0e-5 \
+  --weight-decay 1e-2 \
+  --clip-grad 1.0 \
+  --lr-warmup-fraction .01 \
+  --checkpoint-activations \
+  --log-interval 1 \
+  --save-interval 30000 \
+  --eval-interval 1000 \
+  --eval-iters 10 \
+  --fp16 \
+  --use-mpi \
+  --log-batch-size-to-tensorboard \
+  --log-validation-ppl-to-tensorboard \
+  --wandb-name "gpt_6.7b_${NNODES}node_dp${DP_SIZE}-tp${TP_SIZE}-pp${PP_SIZE}-mpirun-mdx"
