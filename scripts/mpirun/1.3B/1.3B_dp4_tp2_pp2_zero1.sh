eaa1002d6ddebcc2eea668d5a05cf37254481c37
kazuki <kaz.tokyo.tech20@gmail.com>
2023-07-23 00:14:53 +0900

    Revert "fix: change checkpoint path"
    This reverts commit 0e14c5cae394b4422c313a25a06b12c5da0c6e94.

--- scripts/mpirun/1.3B/1.3B_dp4_tp2_pp2_zero1.sh
+++ scripts/mpirun/1.3B/1.3B_dp4_tp2_pp2_zero1.sh
@@ -125,7 +125,7 @@ jobname="${jobname}_seed${seed}_rebase"
 
 output_home="outputs"
 log_path="${output_home}/log/"
-checkpoint_path="/groups/gcf51174/checkpoints/new-megatron-deepspeed/1.3B/${jobname}"
+checkpoint_path="${output_home}/checkpoint/${jobname}"
 ## Microsoft internal constraint: because tensorboard is logged by last rank,
 ## it's better to put the path in NFS instead of Blob.
 tensorboard_dir="${output_home}/tensorboard/"
